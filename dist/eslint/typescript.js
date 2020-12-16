3c7e1a4ca210fd507917df75a2cc977b4d273dad
libo <libo@tuya.com>
2020-12-16 23:41:43 +0800

    feat: 初始化

---
+++ dist/eslint/typescript.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "@typescript-eslint/explicit-function-return-type": 0,
+  "@typescript-eslint/no-var-requires": 0,
+  "@typescript-eslint/interface-name-prefix": 0,
+  "@typescript-eslint/no-use-before-define": 0,
+};
