e4781b6c9cc5a827b8274f4fd0dd2d81c0206520
libo <42805389+libo-FE@users.noreply.github.com>
2021-12-16 16:07:17 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,8 @@ const eslint = require('./eslint');
 const stylelint = require('./stylelint');
 const prettier = require('./prettier');
 
+console.log('--->', "{{name}}", "{{author}}")
+
 module.exports = {
   eslint,
   stylelint,
